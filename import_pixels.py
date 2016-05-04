37bdd14be47ea011cf46710399e1d2a17e133087
Julia S <j.starostenko@gmail.com>
2016-05-04 09:19:45 -0400

    tabs edited to four spaces

--- import_pixels.py
+++ import_pixels.py
@@ -1,17 +1,17 @@
 from PIL import Image
 import numpy as np
 
-def imp_pixels(file_name):
-	def create_pixel_array(file_name):
-		with open(file_name) as f: #when this is closed, image is closed
-			image = Image.open(f)
-			return np.array(image, dtype = np.float64)
+def imp_pixels(filename):
+    def create_pixel_array(filename):
+        with open(filename) as f:
+            image = Image.open(f)
+            return np.array(image, dtype = np.float64)
 
-	pixel_array = create_pixel_array(file_name)
+    pixel_array = create_pixel_array(filename)
 
-	a = []
-	for i in pixel_array:
-		for j in i:
-			a.append(j)
+    a = []
+    for i in pixel_array:
+        for j in i:
+            a.append(j)
 
-	return a		
+    return a
